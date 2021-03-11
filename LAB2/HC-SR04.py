abd8e4de30a37aec0bf17fb661537ee6bda98ed1
chilin-pi <sky707070.cv07@nctu.edu.tw>
2021-03-12 01:49:51 +0800

    update LAB2

--- LAB2/HC-SR04.py
+++ LAB2/HC-SR04.py
@@ -10,24 +10,31 @@ def gpio_setup():
     RPi.GPIO.setup(ECHO, RPi.GPIO.IN)
 
 def hcsr04_setup():
-    Rpi.GPIO.output(TRIGER,1)
+    RPi.GPIO.output(TRIGER,1)
     time.sleep(0.00001)
-    Rpi.GPIO.output(TRIGER,0)
+    RPi.GPIO.output(TRIGER,0)
 
-def sol(start,end):
+def sol(start,end,speed):
     speed /= 2.0
     time = float(end-start)
     return speed*time*100.0
 
 def gpio_shutdown():
-    RPi.GPIO.clearup()
+    RPi.GPIO.cleanup()
 
 if __name__=="__main__":
-    gpio_setup()
-    hcsr04_setup()
-    while(RPi.GPIO.input(ECHO) == RPi.GPIO.LOW):
-        start = time.time()
-    while(RPi.GPIO.input(ECHO) == RPi.GPIO.HIGH):
-        end  = time.time()
-    print("distance is %.2f", sol(start,end))
-    gpio_shutdown()
+    print("go")
+    try:
+        gpio_setup()
+        hcsr04_setup()
+        print("gogo")
+        while(RPi.GPIO.input(ECHO) == RPi.GPIO.LOW):
+            start = time.time()
+        print("start")
+        while(RPi.GPIO.input(ECHO) == RPi.GPIO.HIGH):
+            end  = time.time()
+        print("distance is %.2f", sol(start,end,340.0))
+    except:
+        print("stop by ctrl-c")
+    finally:    
+        gpio_shutdown()
