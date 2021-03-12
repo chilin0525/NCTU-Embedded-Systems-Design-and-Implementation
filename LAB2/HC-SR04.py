0c2811e24ecc28476b59754965fa2a0bc4096582
Chilin Ubuntu 20.04 <sky707070.cv07@nctu.edu.tw>
2021-03-12 16:41:48 +0800

    Update LAB2

--- LAB2/HC-SR04.py
+++ LAB2/HC-SR04.py
@@ -1,41 +1,58 @@
 import RPi.GPIO as GPIO
 import time
+import cv2 as cv
 
-tripin   = 16
-echopin  = 18
+tripin = 16
+echopin = 18
+led = 12
+
 
 def gpio_setup():
     GPIO.setmode(GPIO.BOARD)
     GPIO.setup(tripin, GPIO.OUT)
     GPIO.setup(echopin, GPIO.IN)
+    GPIO.setup(led, GPIO.OUT)
+
 
 def hcsr04_setup():
     GPIO.output(tripin, GPIO.HIGH)
     time.sleep(0.00001)
     GPIO.output(tripin, GPIO.LOW)
 
-def sol(start,end,speed):
+
+def sol(start, end, speed):
     speed /= 2.0
     time = float(end-start)
     return speed*time*100.0
 
+
+def slow():
+    GPIO.output(led, GPIO.HIGH)
+    time.sleep(0.001)
+    GPIO.output(led, GPIO.LOW)
+
+
+def slow():
+    GPIO.output(led, GPIO.HIGH)
+    time.sleep(0.00001)
+    GPIO.output(led, GPIO.LOW)
+
+
 def gpio_shutdown():
     GPIO.cleanup()
 
-if __name__=="__main__":
-    print("go")
-    try:
+
+if __name__ == "__main__":
+    while(1):
         gpio_setup()
         hcsr04_setup()
-        print("gogo")
         while(GPIO.input(echopin) == GPIO.LOW):
-            print("bug")
             start = time.time()
-        print("start")
         while(GPIO.input(echopin) == GPIO.HIGH):
-            end  = time.time()
-        print("distance is %.2f", sol(start,end,340.0))
-    except:
-        print("stop by ctrl-c")
-    finally:    
-        gpio_shutdown()
+            end = time.time()
+        distance = sol(start, end, 340.0)
+        print("distance is " + str(distance))
+        if cv.waitKey(0) & 0xFF == ord('q'):
+            break
+    
+    gpio_shutdown()
--- LAB2/dht.py
+++ LAB2/dht.py
@@ -1,65 +1,55 @@
-# import time
-# import RPi.GPIO as GPIO
+# SPDX-FileCopyrightText: 2021 ladyada for Adafruit Industries
+# SPDX-License-Identifier: MIT
 
-# """
-# VCC: pin2
-# GND: pin6
-# data: pin16
-# """
-
-# dataPin = 16
-
-# def gpio_init():
-#     GPIO.setmode(GPIO.BOARD)
-
-# def dht11_init():
-#     GPIO.setup(dataPin, GPIO.HIGH)
-#     time.sleep(0.05)
-#     GPIO.setup(dataPin,GPIO.LOW)
-#     time.sleep(0.02)
-#     GPIO.setup(dataPin, GPIO.HIGH)
-
-# def dht11_res():
-#     GPIO.set
-
-# def readdata():
-#     pass
-
-# """
-# RH int + dec
-# T  int + dec
-# checksum
-# """
-
-# if __name__=="__main__":
-#     gpio_init()
-#     dht11_init()
-#     (RH_int,RH_dec,T_int,T_dec,checksum) = readdata()
-
-
-import dht11 as dht
+import time
+import board
+import adafruit_dht
 import RPi.GPIO as GPIO
 
-dataPin = 16
+# Initial the dht device, with data pin connected to:
+dhtDevice = adafruit_dht.DHT11(board.D18)
+
+# you can pass DHT22 use_pulseio=False if you wouldn't like to use pulseio.
+# This may be necessary on a Linux single board computer like the Raspberry Pi,
+# but it will not work in CircuitPython.
+# dhtDevice = adafruit_dht.DHT22(board.D18, use_pulseio=False)
+
+LED_PIN = 15
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(LED_PIN, GPIO.OUT)
+GPIO.output(LED_PIN, GPIO.LOW)
 
 
-def gpio_init():
-    GPIO.setwarnings(False)
-    GPIO.setmode(GPIO.BOARD)
+def turnon():
+    GPIO.output(LED_PIN, GPIO.HIGH)
 
-def dht_init():
-    instance = dht11.DHT11(pin=dataPin)
-    return instance
 
-if __name__ == "__main__":
+def turnoff():
+    GPIO.output(LED_PIN, GPIO.LOW)
+
+
+while True:
     try:
-        gpio_init()
-        data = dht_init()
-        if(data.is_valid()):
-            print("Temp:%d, Hum:%d", data.temperature, data.humidity)
+        temperature_c = dhtDevice.temperature
+        temperature_f = temperature_c * (9 / 5) + 32
+        humidity = dhtDevice.humidity
+        print(
+            "Temp: {:.1f} F / {:.1f} C    Humidity: {}% ".format(
+                temperature_f, temperature_c, humidity
+            )
+        )
+        if(temperature_c >= 26):
+            turnon()
         else:
-            print("error of reading data")
-    except:
-        print("error")
-    finally:
-        GPIO.cleanup()
+            turnoff()
+
+    except RuntimeError as error:
+        # Errors happen fairly often, DHT's are hard to read, just keep going
+        print(error.args[0])
+        time.sleep(2.0)
+        continue
+    except Exception as error:
+        dhtDevice.exit()
+        raise error
+
+    time.sleep(2.0)
