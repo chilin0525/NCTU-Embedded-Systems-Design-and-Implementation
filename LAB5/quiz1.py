c936252ad65d8d58526aabcb6794954603c65d1e
Chilin Ubuntu 20.04 <sky707070.cv07@nctu.edu.tw>
2021-04-09 13:22:24 +0800

    Update LAB5

--- LAB5/quiz1.py
+++ LAB5/quiz1.py
@@ -1,12 +1,18 @@
 import time
 import picamera
-with picamera.PiCamera() as camera:
-    camera.start_preview()
-    try:
-        for i, filename in enumerate(camera.capture_continuous('{counter:03d}.jpg')):
-            print(filename)
-            time.sleep(1)
-            if i == 59:
-                break
-    finally:
-        camera.stop_preview()
+import schedule
+import datetime
+
+def job():
+    with picamera.PiCamera() as camera:
+        camera.start_preview()
+        try:
+            for i, filename in enumerate(camera.capture_continuous('{counter:03d}.jpg')):
+                print(filename)
+                time.sleep(1)
+                if i == 59:
+                    break
+        finally:
+            camera.stop_preview()
+
+print(datetime.datetime.now())
