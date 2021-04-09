5d3d88601775a73a89e372d2475c3b05dbe6f1c5
Chilin Ubuntu 20.04 <sky707070.cv07@nctu.edu.tw>
2021-04-09 12:46:13 +0800

    Update LAB5

--- LAB5/quiz1.py
+++ LAB5/quiz1.py
@@ -6,7 +6,7 @@ with picamera.PiCamera() as camera:
         for i, filename in enumerate(camera.capture_continuous('image{counter:02d}.jpg')):
             print(filename)
             time.sleep(1)
-    if i == 59:
-        break
+            if i == 59:
+                break
     finally:
         camera.stop_preview()
