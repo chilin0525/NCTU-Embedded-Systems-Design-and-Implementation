e393d4e31dee3b11fb56f78a9fb664187ee2897b
Chilin Ubuntu 20.04 <sky707070.cv07@nctu.edu.tw>
2021-04-16 00:39:33 +0800

    Update LAB5

--- LAB5/hw5-1.py
+++ LAB5/hw5-1.py
@@ -6,7 +6,7 @@ def job():
     with picamera.PiCamera() as camera:
         camera.start_preview()
         try:
-            for i, filename in enumerate(camera.capture_continuous('image{counter:02d}.jpg')):
+            for i, filename in enumerate(camera.capture_continuous('image{timestamp:%Y%m%d}{counter:02d}.jpg')):
                 print(filename)
                 time.sleep(1)
                 if i == 9:
@@ -15,7 +15,7 @@ def job():
             camera.stop_preview()
 
 
-schedule.every().day.at("00:34").do(job)
+schedule.every().day.at("00:40").do(job)
 while True:
     schedule.run_pending()
     time.sleep(1)
