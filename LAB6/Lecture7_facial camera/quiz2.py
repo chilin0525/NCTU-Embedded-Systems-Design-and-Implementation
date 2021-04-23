a9165549e7e44a42bce0dfe6b32855cf705ba20e
Chilin Ubuntu 20.04 <sky707070.cv07@nctu.edu.tw>
2021-04-23 12:55:58 +0800

    update LAB6

--- LAB6/Lecture7_facial camera/quiz2.py
+++ LAB6/Lecture7_facial camera/quiz2.py
@@ -1,77 +1,67 @@
-#!/usr/bin/python
-#+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
-#|R|a|s|p|b|e|r|r|y|P|i|.|c|o|m|.|t|w|
-#+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
-# Copyright (c) 2014, raspberrypi.com.tw
-# All rights reserved.
-# Use of this source code is governed by a BSD-style license that can be
-# found in the LICENSE file.
-#
-# Author : sosorry
-# Date   : 05/31/2015
-# Origin : http://blog.miguelgrinberg.com/post/video-streaming-with-flask
-
-import cv2
+from imutils import face_utils
+import numpy as np
 import imutils
+import dlib
+import cv2
 import time
 from imutils.video.pivideostream import PiVideoStream
 from imutils.video import FPS
 
+# load OpenCV's Haar cascade for face detection,
+# (This is faster than dlib's built-in HOG detector, but less accurate.)
+detector_file = "model/haarcascade_frontalface_default.xml"
+detector = cv2.CascadeClassifier(detector_file)
 
-class Camera(object):
-    def __init__(self):
-        """if cv2.__version__.startswith('2'):
-            PROP_FRAME_WIDTH = cv2.cv.CV_CAP_PROP_FRAME_WIDTH
-            PROP_FRAME_HEIGHT = cv2.cv.CV_CAP_PROP_FRAME_HEIGHT
-        else:
-            PROP_FRAME_WIDTH = cv2.CAP_PROP_FRAME_WIDTH
-            PROP_FRAME_HEIGHT = cv2.CAP_PROP_FRAME_HEIGHT"""
+# create the facial landmark predictor.
+predictor_file = "model/shape_predictor_68_face_landmarks.dat"
+predictor = dlib.shape_predictor(predictor_file)
 
-        self.video = PiVideoStream().start()
-        time.sleep(2.0)
-        #self.video.set(PROP_FRAME_WIDTH, 640)
-        #self.video.set(PROP_FRAME_HEIGHT, 480)
-        #self.video.set(PROP_FRAME_WIDTH, 320)
-        #self.video.set(PROP_FRAME_HEIGHT, 240)
+vs = PiVideoStream().start()
+time.sleep(2.0)
 
-    #def __del__(self):
-     #   self.video.release()
+while True:
+    # load the input image, resize it, and convert it to grayscale
+    frame = vs.read()
+    image = imutils.resize(frame, width=500)
 
-    def get_frame(self):
-        image = self.video.read()
+    # cvtColor: Converts an image from one color space to another.
+    # Here, convert a RGB image to gray
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
-        #mage = imutils.resize(image, width=400)
-        gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    # detect faces in the grayscale frame by opencv's method
+    rects = detector.detectMultiScale(gray, scaleFactor=1.1,
+                                  minNeighbors=5, minSize=(30, 30),
+                                  flags=cv2.CASCADE_SCALE_IMAGE)
 
-        detector = dlib.get_frontal_face_detector()
+    # loop over the face detections
+    face_counter = 0
+    for (x, y, w, h) in rects:
+        # construct a dlib rectangle object from the Haar cascade bounding box
+        rect = dlib.rectangle(int(x), int(y), int(x + w), int(y + h))
 
-        predictor_file = "model/shape_predictor_68_face_landmarks.dat"
-        predictor = dlib.shape_predictor(predictor_file)
+    # determine the facial landmarks for the face region, then
+    # convert the facial landmark (x, y)-coordinates to a NumPy
+    # array
+    shape = predictor(gray, rect)
+    shape = face_utils.shape_to_np(shape)
 
+    # convert dlib's rectangle to a OpenCV-style bounding box
+    # [i.e., (x, y, w, h)], then draw the face bounding box
+    (x, y, w, h) = face_utils.rect_to_bb(rect)
+    cv2.rectangle(image, (x, y), (x + w, y + h), (0, 255, 0), 2)
 
-        rects = detector(gray, 1)
-        
-        for (i, rect) in enumerate(rects):
-        	# determine the facial landmarks for the face region, then
-        	# convert the facial landmark (x, y)-coordinates to a NumPy
-        	# array
-            shape = predictor(gray, rect)
-            shape = face_utils.shape_to_np(shape)
+    # show the face number
+    cv2.putText(image, "Face #{}".format(face_counter + 1), (x - 10, y - 10),
+                cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
 
-            # convert dlib's rectangle to a OpenCV-style bounding box
-            # [i.e., (x, y, w, h)], then draw the face bounding box
-            (x, y, w, h) = face_utils.rect_to_bb(rect)
-            cv2.rectangle(image, (x, y), (x + w, y + h), (0, 255, 0), 2)
+    # loop over the (x, y)-coordinates for the facial landmarks
+    # and draw them on the image
+    for (x, y) in shape:
+        cv2.circle(image, (x, y), 1, (0, 0, 255), -1)
 
-            # show the face number
-            cv2.putText(image, "Face #{}".format(i + 1), (x - 10, y - 10),
-                    cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
+    face_counter = face_counter + 1
 
-            # loop over the (x, y)-coordinates for the facial landmarks
-            # and draw them on the image
-            for (x, y) in shape:
-                cv2.circle(image, (x, y), 1, (0, 0, 255), -1)
-
-
-        ret, jpeg = cv2.imencode('.jpg', image)
-        return jpeg.tostring()
+    # show the output image with the face detections + facial landmarks
+    key = cv2.waitKey(1) & 0xFF
+    if key == ord("q"):
+        break
