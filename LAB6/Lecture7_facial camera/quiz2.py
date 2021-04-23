eee199ef780ee0012072810697738504e7074e0c
Chilin Ubuntu 20.04 <sky707070.cv07@nctu.edu.tw>
2021-04-23 12:57:28 +0800

    update LAB6

--- LAB6/Lecture7_facial camera/quiz2.py
+++ LAB6/Lecture7_facial camera/quiz2.py
@@ -39,27 +39,27 @@ while True:
         # construct a dlib rectangle object from the Haar cascade bounding box
         rect = dlib.rectangle(int(x), int(y), int(x + w), int(y + h))
 
-    # determine the facial landmarks for the face region, then
-    # convert the facial landmark (x, y)-coordinates to a NumPy
-    # array
-    shape = predictor(gray, rect)
-    shape = face_utils.shape_to_np(shape)
+        # determine the facial landmarks for the face region, then
+        # convert the facial landmark (x, y)-coordinates to a NumPy
+        # array
+        shape = predictor(gray, rect)
+        shape = face_utils.shape_to_np(shape)
 
-    # convert dlib's rectangle to a OpenCV-style bounding box
-    # [i.e., (x, y, w, h)], then draw the face bounding box
-    (x, y, w, h) = face_utils.rect_to_bb(rect)
-    cv2.rectangle(image, (x, y), (x + w, y + h), (0, 255, 0), 2)
+        # convert dlib's rectangle to a OpenCV-style bounding box
+        # [i.e., (x, y, w, h)], then draw the face bounding box
+        (x, y, w, h) = face_utils.rect_to_bb(rect)
+        cv2.rectangle(image, (x, y), (x + w, y + h), (0, 255, 0), 2)
 
-    # show the face number
-    cv2.putText(image, "Face #{}".format(face_counter + 1), (x - 10, y - 10),
-                cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
+        # show the face number
+        cv2.putText(image, "Face #{}".format(face_counter + 1), (x - 10, y - 10),
+                    cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
 
-    # loop over the (x, y)-coordinates for the facial landmarks
-    # and draw them on the image
-    for (x, y) in shape:
-        cv2.circle(image, (x, y), 1, (0, 0, 255), -1)
+        # loop over the (x, y)-coordinates for the facial landmarks
+        # and draw them on the image
+        for (x, y) in shape:
+            cv2.circle(image, (x, y), 1, (0, 0, 255), -1)
 
-    face_counter = face_counter + 1
+        face_counter = face_counter + 1
 
     # show the output image with the face detections + facial landmarks
     key = cv2.waitKey(1) & 0xFF
