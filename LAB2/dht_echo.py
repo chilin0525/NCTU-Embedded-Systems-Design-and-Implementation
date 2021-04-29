e8c5fa00d76ddab7cf7e2163ba8557a9fa594508
Chilin Ubuntu 20.04 <sky707070.cv07@nctu.edu.tw>
2021-04-29 21:49:31 +0800

    update LAB2

--- LAB2/dht_echo.py
+++ LAB2/dht_echo.py
@@ -4,19 +4,18 @@
 import time
 import board
 import adafruit_dht
-import RPi.GPIO as GPIO
 
 # Initial the dht device, with data pin connected to:
-dhtDevice = adafruit_dht.DHT11(board.D4)
+dhtDevice = adafruit_dht.DHT22(board.D18)
 
 # you can pass DHT22 use_pulseio=False if you wouldn't like to use pulseio.
 # This may be necessary on a Linux single board computer like the Raspberry Pi,
 # but it will not work in CircuitPython.
 # dhtDevice = adafruit_dht.DHT22(board.D18, use_pulseio=False)
 
-
 while True:
     try:
+        # Print the values to the serial port
         temperature_c = dhtDevice.temperature
         temperature_f = temperature_c * (9 / 5) + 32
         humidity = dhtDevice.humidity
