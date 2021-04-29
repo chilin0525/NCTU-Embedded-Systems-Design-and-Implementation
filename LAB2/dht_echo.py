5af369f79c96144fb09ce664c40e2f79504eb1ba
Chilin Ubuntu 20.04 <sky707070.cv07@nctu.edu.tw>
2021-04-29 21:42:52 +0800

    update LAB2

--- LAB2/dht_echo.py
+++ LAB2/dht_echo.py
@@ -7,7 +7,7 @@ import adafruit_dht
 import RPi.GPIO as GPIO
 
 # Initial the dht device, with data pin connected to:
-dhtDevice = adafruit_dht.DHT11(board.D18)
+dhtDevice = adafruit_dht.DHT11(board.D4)
 
 # you can pass DHT22 use_pulseio=False if you wouldn't like to use pulseio.
 # This may be necessary on a Linux single board computer like the Raspberry Pi,
