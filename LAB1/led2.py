7cf96833f87a4b0edc4d4598598c8f7779e04d4e
school ip <chilin.cs07@nycu.edu.tw>
2021-04-30 12:43:34 +0800

    update LAB1

---
+++ LAB1/led2.py
@@ -0,0 +1,16 @@
+import RPi.GPIO as GPIO
+import time
+
+LED_PIN = 15
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(LED_PIN, GPIO.OUT)
+
+try:
+    while True:
+        print("LED is on")
+        GPIO.output(LED_PIN, GPIO.HIGH)
+        time.sleep(1)
+        print("LED is off")
+        GPIO.output(LED_PIN, GPIO.LOW)
+        time.sleep(1)
+
